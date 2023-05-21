0c848a0db0816da798fbd7eb3f19d35f2faddc6e
gawryllah <gawrysjak@gmail.com>
2023-05-22 01:34:33 +0200

    Changes

--- react_01/src/App.js
+++ react_01/src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function updateRevList(list){
+ 
+  return [...list].reverse()
+}
 
 function App() {
+  const xd = [
+    {text: "aaa", id: 1, color: "red"},
+    {text: "bbb", id: 2, color: "blue"},
+    {text: "ccc", id: 3, color:"green"}
+            ] 
+  const [testVals, setTestVals] = useState(xd);
+  const [toggle, setToggle] = useState(false)
+  
+    const btn = <button onClick={() => alert("test msg")}>Click</button>
+    const toggleBtn = <button onClick={() => setToggle(!toggle)}>Wlacz/Wylacz</button>
+    const revListBtn = <button onClick={() => {setTestVals(updateRevList(testVals))}}>Reverse List</button>
+
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <body>
+          {toggleBtn}
+          {toggle ? "On" : "Off"}
+          {toggle && (
+          <div>
+          <ul>
+            {testVals.map(x => (<li key={x.id} style={{color: x.style}}>{x.text}</li> ))}
+          </ul>
+          {btn}
+          {revListBtn}
+          </div>
+          )}
+        </body>
       </header>
     </div>
+    </>
   );
 }
 
